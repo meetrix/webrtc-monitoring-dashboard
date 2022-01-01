e7fb9aec8ade8ee6b933a66da2910090f0afa3c2
Buddhika Jayawardhana <buddhika.anushka@gmail.com>
2022-01-01 12:18:56 +0000

    Show stats

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Counter } from './features/counter/Counter';
 import Clients from './features/clients/Clients';
+import Debugger from './features/debugger/Debugger';
 
 const App = (): React.ReactElement => {
   return (
@@ -9,6 +10,7 @@ const App = (): React.ReactElement => {
       <Routes>
         <Route path="/" element={<Counter />} />
         <Route path="/clients" element={<Clients />} />
+        <Route path="/debugger" element={<Debugger />} />
       </Routes>
     </BrowserRouter>
   );
--- src/features/debugger/debuggerSlice.ts
+++ src/features/debugger/debuggerSlice.ts
@@ -1,14 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { Peer } from '@meetrix/webrtc-monitoring-common-lib';
 import peers from '../../mocks/peers';
+import { RootState } from '../../app/store';
 import { getUrlParams } from '../../utils/urlUtils';
 
 const { mockStats } = getUrlParams();
-const initialState = mockStats ? peers : {};
+const initialState = {
+  peers: mockStats ? (peers as Peer[]) : undefined,
+};
+
+export type PeersType = Peer[] | undefined;
 
 export const debuggerSlice = createSlice({
   name: 'debugger',
   initialState,
   reducers: {},
 });
+export const selectPeers = (state: RootState): PeersType =>
+  state?.debugger?.peers;
 
 export default debuggerSlice.reducer;
