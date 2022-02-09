4c871b32cb4227b053744e326798b452375b85e7
Dilushan <dilushan@meetrix.io>
2022-02-09 08:21:00 +0530

    rename env variables

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export default {
     baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:9100/v1',
   },
   socket: {
-    url: process.env.REACT_APP_API_BASE_USER_URL || 'http://localhost:9100',
+    url: process.env.REACT_APP_SOCKET_BASE_URL || 'http://localhost:9100',
     options: {
       path: '/stats',
       auth: {
