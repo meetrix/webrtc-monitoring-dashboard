da95f95e7e54d26e1c35505f387b28a88b7eb66a
Kaveesha Tharushika <kaveesha@meetrix.io>
2022-04-25 11:22:14 +0530

    Removed 'dx-react-chart'

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,33 +1,11 @@
-import React, { memo, useEffect } from 'react';
-import clsx from 'clsx';
-import { DataGrid } from '@mui/x-data-grid';
+import React, { memo } from 'react';
 import { ButtonProps, Theme } from '@mui/material';
 import { withStyles, WithStyles, createStyles } from '@mui/styles';
-import {
-  ArgumentAxis,
-  ValueAxis,
-  Chart,
-  LineSeries,
-  SplineSeries,
-} from '@devexpress/dx-react-chart-material-ui';
 
 const styles = (theme: Theme) => {
   return createStyles({
     root: {},
   });
-};
-
-const generateData = (start: any, end: any, step: any) => {
-  const data = [];
-  for (let i = start; i < end; i += step) {
-    data.push({
-      splineValue: Math.sin(i) / i,
-      lineValue: (i / 15) ** 2.718 - 0.2,
-      argument: i,
-    });
-  }
-
-  return data;
 };
 
 export interface ChartComponentProps
@@ -47,11 +25,9 @@ export const ChartComponent: React.FC<ChartComponentProps> = ({
   // }, []);
 
   return (
-    <Chart {...otherProps}>
-      <ArgumentAxis />
-      <ValueAxis />
-      <LineSeries valueField="value" argumentField="argument" />
-    </Chart>
+    <div>
+      <div>Chart</div>
+    </div>
   );
 };
 
