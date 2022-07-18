1cb6ffbc649410fe64a2d7e0ee26d731255e7e93
Kaveesha Tharushika <kaveesha@meetrix.io>
2022-07-18 12:17:13 +0530

    Google analytics and tag manager

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import ReactGA from 'react-ga';
+import TagManager from 'react-gtm-module';
 import { AppContainer } from './features/app';
 import { MainLayout } from './components/layout';
 import Routes from './routes';
 
 const App = (): React.ReactElement => {
+  const tagManagerArgs = {
+    gtmId: 'GTM-N8NWRND',
+  };
+
+  useEffect(() => {
+    ReactGA.initialize('UA-118777783-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+
   return (
     <MainLayout>
       <AppContainer />
